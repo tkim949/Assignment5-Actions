20edd158839214201fd37219736ad3bee5a8f33d
tkim949 <tkim949@gmail.com>
2020-02-27 14:26:49 -0800

    add function 1

--- task.py
+++ task.py
@@ -1,3 +1,10 @@
+import math
+
 
 def firstrun():
     return "success"
+
+
+def getArea(radius):
+    PI = 3.142
+    return int(PI * (radius*radius)*100)/100
--- tests.py
+++ tests.py
@@ -12,6 +12,16 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test1_getArea(self):
+        radius = 5.6
+        result = task.getArea(radius)
+        self.assertEqual(98.53, result)
+
+    def test2_getArea(self):
+        radius = 3
+        result = task.getArea(radius)
+        self.assertNotEqual(3, result)
+
 
 if __name__ == '__main__':
     unittest.main()
