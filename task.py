6c4043652b7ed150da76fbce45f5163da04c7433
tkim949 <tkim949@gmail.com>
2020-02-28 09:42:54 -0800

    Add the first function

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 # import math
+# from datetime import date
 
 
 def firstrun():
@@ -8,3 +9,19 @@ def firstrun():
 def getArea(radius):
     PI = 3.142
     return int(PI * (radius*radius)*100)/100
+
+
+def getElementList(aList):
+
+    resList = []
+    resList.append(aList[0])
+    resList.append(aList[-1])
+
+    return resList
+
+
+def daysBWdates(date1, date2):
+
+    delta = abs(date2 - date1)
+
+    return delta.days
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+# from datetime import date
 
 
 class TestCase(unittest.TestCase):
@@ -21,6 +22,34 @@ class TestCase(unittest.TestCase):
         radius = 3
         result = task.getArea(radius)
         self.assertNotEqual(3, result)
+    '''
+    def test1_getElementList(self):
+        testList1 = [1, 2, 3, 4, 5, 6, 0]
+        gotList = task.getElementList(testList1)
+        self.assertEqual(1, gotList[0])
+        self.assertEqual(0, gotList[-1])
+
+    def test2_getElementList(self):
+        testList2 = ['apple', 'banana', 'orange', 'mango', 'grape']
+        gotList = task.getElementList(testList2)
+        self.assertNotEqual('banana', gotList[0])
+        self.assertNotEqual('apple', gotList[-1])
+
+    def test1_getdaysBWdates(self):
+        date1 = date(2011, 4, 8)
+        date2 = date(2020, 2, 27)
+        days = task.daysBWdates(date1, date2)
+
+        self.assertEqual(3247, days)
+
+    def test2_getdaysBWdates(self):
+        date1 = date(2020, 2, 25)
+        date2 = date(2020, 2, 27)
+
+        days = task.daysBWdates(date1, date2)
+
+        self.assertNotEqual(10, days)
+    '''
 
 
 if __name__ == '__main__':
